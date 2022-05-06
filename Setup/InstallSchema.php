b0fb05aa077f6387ce7c5d01b5963c844799a7c5
Petar <petar@customgento.com>
2022-05-06 15:43:59 +0200

    Initial commit

---
+++ Setup/InstallSchema.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Test\Module\Setup;
+
+use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\DB\Ddl\Table;
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Zend_Db_Exception;
+
+/**
+ * Class InstallSchema
+ */
+class InstallSchema implements InstallSchemaInterface
+{
+    /**
+     * @param SchemaSetupInterface   $setup
+     * @param ModuleContextInterface $context
+     *
+     * @throws Zend_Db_Exception
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+
+        $installer->startSetup();
+
+            $table = $installer->getConnection()
+                ->newTable($installer->getTable('test_table'))
+                ->addColumn('post_id', Table::TYPE_INTEGER, null, [
+                    'identity' => true,
+                    'nullable' => false,
+                    'primary'  => true,
+                    'unsigned' => true,
+                ], 'Post ID');
+
+            $installer->getConnection()->createTable($table);
+
+        $installer->endSetup();
+    }
+}
